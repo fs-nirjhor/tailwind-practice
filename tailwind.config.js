e53a4da55bae8fd6311191f68373a598c8579fc1
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-06-21 15:15:15 +0600

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js}",
-  ],
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        leafGreen: colors.teal,
+        soil: "#a66205",
+        ash: {
+          light: "#a29898",
+          DEFAULT: "#7d854e",
+          500: "#545979",
+          dark: "#45254a",
+        },
+      },
+      fontFamily: {
+        sans: ["Josefin Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
